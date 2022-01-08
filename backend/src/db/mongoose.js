f2fa472106db8a0b3478b770fb1bfc5fb5855f06
G82DOOM <71901005+G82DOOM@users.noreply.github.com>
2022-01-08 16:18:00 +0200

    Add files via upload

---
+++ backend/src/db/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config;
+mongoose.connect('mongodb://mongo:mongo@'+process.env.MONGODB_SERVICE_HOST+':27017/todoApp', 
+{   
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("connected to database");
+}).catch(error => {
+    console.log("unable to connect to database",error);
+})
